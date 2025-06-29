cbdb6c0ffbc708d6c0f44a8c80eb235723c3653a
v-aravindcharyn <v-aravindchary.n@aims360.com>
2025-06-29 19:46:15 +0530

    Updated all files with latest changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className=" bg-black text-white text-left text-2xl transition-opacity duration-200 hover:opacity-20 py-5 px-10">
-      Granny’s Pantry
+      Granny’s Pantry Home
     </header>
   );
 }
